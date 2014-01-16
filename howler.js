41ef5c102f980d390a9da5d7c76400d38dffd7da
Michal Kuklis <michal.kuklis@gmail.com>
2014-01-16 11:36:38 -0500

    Added CommonJS support.

--- howler.js
+++ howler.js
@@ -1192,6 +1192,13 @@
       };
     });
   }
+  /**
+   * Add support for CommonJS libraries such as browserify.
+   */
+  else if (typeof exports !== 'undefined') {
+    exports.Howler = Howler;
+    exports.Howl = Howl;
+  }
   
   // define globally in case AMD is not available or available but not used
   window.Howler = Howler;
