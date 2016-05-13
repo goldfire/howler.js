8c00ab2d36289aa322d7f3f4783761018d19b113
James Simpson <james33@gmail.com>
2016-05-13 16:56:55 -0500

    v2.0.0-beta13

--- src/howler.core.js
+++ src/howler.core.js
@@ -1,5 +1,5 @@
 /*!
- *  howler.js v2.0.0-beta12
+ *  howler.js v2.0.0-beta13
  *  howlerjs.com
  *
  *  (c) 2013-2016, James Simpson of GoldFire Studios
--- src/plugins/howler.effects.js
+++ src/plugins/howler.effects.js
@@ -1,7 +1,7 @@
 /*!
  *  Effects Plugin - Adds advanced Web Audio API functionality.
  *  
- *  howler.js v2.0.0-beta12
+ *  howler.js v2.0.0-beta13
  *  howlerjs.com
  *
  *  (c) 2013-2016, James Simpson of GoldFire Studios
