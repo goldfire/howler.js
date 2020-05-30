b95a580ee6077102148979d9d30ab98efab39887
Yisheng Jiang <yishengjiang@yahoo.com>
2020-05-29 21:45:23 -0700

    revert unintended change

--- examples/3d/js/controls.js
+++ examples/3d/js/controls.js
@@ -8,33 +8,27 @@
  *  MIT License
  */
 
-"use strict";
+'use strict';
 
 /**
  * Defines and handles the various controls.
  */
-var Controls = function () {
+var Controls = function() {
   // Define our control key codes and states.
   this.codes = {
     // Arrows
-    37: "left",
-    39: "right",
-    38: "front",
-    40: "back",
+    37: 'left', 39: 'right', 38: 'front', 40: 'back',
     // WASD
-    65: "left",
-    68: "right",
-    87: "front",
-    83: "back",
+    65: 'left', 68: 'right', 87: 'front', 83: 'back',
   };
-  this.states = { left: false, right: false, front: false, back: false };
+  this.states = {left: false, right: false, front: false, back: false};
 
   // Setup the DOM listeners.
-  document.addEventListener("keydown", this.key.bind(this, true), false);
-  document.addEventListener("keyup", this.key.bind(this, false), false);
-  document.addEventListener("touchstart", this.touch.bind(this), false);
-  document.addEventListener("touchmove", this.touch.bind(this), false);
-  document.addEventListener("touchend", this.touchEnd.bind(this), false);
+  document.addEventListener('keydown', this.key.bind(this, true), false);
+  document.addEventListener('keyup', this.key.bind(this, false), false);
+  document.addEventListener('touchstart', this.touch.bind(this), false);
+  document.addEventListener('touchmove', this.touch.bind(this), false);
+  document.addEventListener('touchend', this.touchEnd.bind(this), false);
 };
 Controls.prototype = {
   /**
@@ -42,7 +36,7 @@ Controls.prototype = {
    * @param  {Boolean} pressed Whether or not the key is being pressed.
    * @param  {Object} event   DOM event data including the key being pressed.
    */
-  key: function (pressed, event) {
+  key: function(pressed, event) {
     var state = this.codes[event.keyCode];
 
     if (!state) {
@@ -58,7 +52,7 @@ Controls.prototype = {
    * Listen for touch events and determine which key to simulate.
    * @param  {Object} event DOM event data including the position touched.
    */
-  touch: function (event) {
+  touch: function(event) {
     var touches = event.touches[0];
 
     // Reset the states.
@@ -66,13 +60,13 @@ Controls.prototype = {
 
     // Determine which key to simulate.
     if (touches.pageY < window.innerHeight * 0.3) {
-      this.key(true, { keyCode: 38 });
+      this.key(true, {keyCode: 38});
     } else if (touches.pageY > window.innerHeight * 0.7) {
-      this.key(true, { keyCode: 40 });
+      this.key(true, {keyCode: 40});
     } else if (touches.pageX < window.innerWidth * 0.5) {
-      this.key(true, { keyCode: 37 });
+      this.key(true, {keyCode: 37});
     } else if (touches.pageX > window.innerWidth * 0.5) {
-      this.key(true, { keyCode: 39 });
+      this.key(true, {keyCode: 39});
     }
   },
 
@@ -80,7 +74,7 @@ Controls.prototype = {
    * Fired to reset all key statuses based on no fingers being on the screen.
    * @param  {Object} event DOM event data including the position touched.
    */
-  touchEnd: function (event) {
+  touchEnd: function(event) {
     this.states.left = false;
     this.states.right = false;
     this.states.front = false;
@@ -88,5 +82,5 @@ Controls.prototype = {
 
     event.preventDefault();
     event.stopPropagation();
-  },
+  }
 };
