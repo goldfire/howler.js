ea3c34adaea7e5eeaea6fc4d8b68bff1d970aa11
Jakob Wierzba <git@jakobwierzba.de>
2021-11-19 10:19:49 +0100

    build dist

--- dist/howler.js
+++ dist/howler.js
@@ -573,11 +573,6 @@
     init: function(o) {
       var self = this;
 
-      // If we don't have an AudioContext created yet, run the setup.
-      if (!Howler.ctx) {
-        setupAudioContext();
-      }
-
       // Setup user-defined default properties.
       self._autoplay = o.autoplay || false;
       self._format = (typeof o.format !== 'string') ? o.format : [o.format];
@@ -595,6 +590,17 @@
         headers: o.xhr && o.xhr.headers ? o.xhr.headers : null,
         withCredentials: o.xhr && o.xhr.withCredentials ? o.xhr.withCredentials : false,
       };
+
+      if (!self._html5) {
+        // If we're not forcing HTML5 and we don't have an AudioContext created yet, run the setup.
+        if (!Howler.ctx) {
+          // sets Howler.usingWebAudio
+          setupAudioContext();
+        }
+      } else {
+        Howler.usingWebAudio = false;
+      }
+      self._webAudio = Howler.usingWebAudio;
 
       // Setup all other default properties.
       self._duration = 0;
@@ -619,9 +625,6 @@
       self._onseek = o.onseek ? [{fn: o.onseek}] : [];
       self._onunlock = o.onunlock ? [{fn: o.onunlock}] : [];
       self._onresume = [];
-
-      // Web Audio or HTML5 Audio?
-      self._webAudio = Howler.usingWebAudio && !self._html5;
 
       // Automatically try to enable audio.
       if (typeof Howler.ctx !== 'undefined' && Howler.ctx && Howler.autoUnlock) {
@@ -2543,6 +2546,7 @@
 
     // Create and expose the master GainNode when using Web Audio (useful for plugins or advanced usage).
     if (Howler.usingWebAudio) {
+      // note: this part breaks iOS 13.3+ ControlCenter notification
       Howler.masterGain = (typeof Howler.ctx.createGain === 'undefined') ? Howler.ctx.createGainNode() : Howler.ctx.createGain();
       Howler.masterGain.gain.setValueAtTime(Howler._muted ? 0 : Howler._volume, Howler.ctx.currentTime);
       Howler.masterGain.connect(Howler.ctx.destination);
