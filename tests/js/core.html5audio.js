508644a5edd875c7c5339d13f88bb65e64d9e979
jbgomez <jgomez@tunein.com>
2022-04-17 14:40:02 -0700

    Consolidate global options parsing in parseGlobalOptions.js

--- tests/js/core.html5audio.js
+++ tests/js/core.html5audio.js
@@ -1,37 +1,3 @@
-// Set global options from query params
-function parseValueFromEntry(entry) {
-  var key = entry[0];
-  var value = entry[1];
-  var parsedValue = value;
-
-  if (value.toLowerCase() === 'true') {
-    parsedValue = true;
-  } else if (value.toLowerCase() === 'false') {
-    parsedValue = false;
-  } else if (!isNaN(value)) {
-    parsedValue = parseFloat(value);
-  }
-
-  return [key, parsedValue];
-}
-
-function setGlobalOptions(entry) {
-  var key = entry[0];
-  var value = entry[1];
-
-  if (Howler.hasOwnProperty(key)) {
-    Howler[key] = value;
-  }
-}
-
-window.location.search
-  .slice(1)
-  .split('&')
-  .filter(function(entry) { return entry[0]; })
-  .map(function(pair) { return pair.split('='); })
-  .map(parseValueFromEntry)
-  .forEach(setGlobalOptions);
-
 // Cache the label for later use.
 var label = document.getElementById('label');
 var start = document.getElementById('start');
